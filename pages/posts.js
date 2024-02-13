599de7fb22b7e198f09c8d6149a1ecc7054dbdb1
Joseph Biden <aseprbc@proton.me>
2024-02-13 09:34:55 +0700

    new posts hehe

--- pages/posts.js
+++ pages/posts.js
@@ -7,6 +7,9 @@ import thumbHowToUseInkdrop from '../public/images/contents/netlifypost3.jpg'
 import thumbFishWorkflow from '../public/images/contents/netlifypost2.jpg'
 import thumbMyDeskSetup from '../public/images/contents/netlifypost.jpg'
 import thumbToba1 from '../public/images/contents/toba1.webp'
+
+import thumbRec1posts from '../public/images/contents/xampp1.jpg'
+import thumbRec2posts from '../public/images/contents/HTML-basic-format.png'
 const Posts = () => (
   <Layout title="Posts">
     <Container>
@@ -38,6 +41,32 @@ const Posts = () => (
           />
         </SimpleGrid>
       </Section>
+
+      <Heading as="h3" fontSize={20} mb={5}>
+        Recommended Posts by me
+      </Heading>
+
+      <Heading as="h3" fontSize={16} mb={7}>
+        <u>Coding</u>
+      </Heading>
+      <Section delay={0.1}>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            title="Cara Menjalankan XAMPP Localhost di Windows"
+            thumbnail={thumbRec1posts}
+            href="https://www.awonapa.com/2021/06/cara-menjalankan-xampp-localhost-di.html"
+          />
+
+          <GridItem
+            title="HTML Introduction - GeeksforGeeks"
+            thumbnail={thumbRec2posts}
+            href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.geeksforgeeks.org%2Fhtml-introduction%2F&psig=AOvVaw320xPtTb2zUyv_o--CfrzO&ust=1707877825107000&source=images&cd=vfe&opi=89978449&ved=0CBQQjhxqFwoTCMCIpuqip4QDFQAAAAAdAAAAABAE"
+          />
+          
+        </SimpleGrid>
+      </Section>
+
+       
 
       
     </Container>
